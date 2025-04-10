27dd1843f43fe159551631a80ac82295393340da
Tony <tony.bruchon@hotmail.fr>
2025-04-10 23:48:23 +0200

    Ajout du contexte PizzaDbContext et configuration d'Entity Framework avec SQL Server
    - Création de la classe PizzaDbContext dans Data/PizzaDbContext.cs
    - Ajout du DbSet<Pizza> pour représenter la table des pizzas
    - Configuration d'Entity Framework Core dans Program.cs
      - Ajout de l'injection de dépendances pour PizzaDbContext
      - Utilisation de SQL Server avec la chaîne de connexion "PizzaDb"

---
+++ Data/PizzaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStoreAPI.Models; 
+
+namespace PizzaStoreAPI.Data
+{
+    public class PizzaDbContext : DbContext
+    {
+        public PizzaDbContext(DbContextOptions<PizzaDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Pizza> Pizzas => Set<Pizza>(); 
+    }
+}
