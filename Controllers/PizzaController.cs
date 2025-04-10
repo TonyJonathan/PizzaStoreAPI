92ababca0b7285b7d12b7848578910840cbecfe3
Tony <tony.bruchon@hotmail.fr>
2025-04-10 17:18:11 +0200

    Added PizzaController with GET /api/pizzas route

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("api/pizzas")]
+public class PizzaController : ControllerBase
+{
+    private static readonly List<string> Pizzas = new()
+    {
+        "Margherita", "Pepperoni", "Reine", "Végétarienne", "4 Fromages"
+    };
+
+    [HttpGet]
+    public IEnumerable<string> GetAll()
+    {
+        return Pizzas;
+    }
+}
