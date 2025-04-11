3eb2db85288dedd4f456ed0172cab994a175d46f
Tony <tony.bruchon@hotmail.fr>
2025-04-11 08:49:45 +0200

    Add GET endpoints to retrieve all pizzas and a specific pizza by ID
    - Implemented GET /Pizza to return all pizzas
    - Implemented GET /Pizza/{id} to return a single pizza or 404 if not found

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,27 +1,41 @@
-﻿using PizzaStoreAPI.Models;
-using PizzaStoreAPI.Services;
-using Microsoft.AspNetCore.Mvc; 
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaStoreAPI.Models;
+using PizzaStoreAPI.Data; 
+using Microsoft.EntityFrameworkCore;
 
-namespace PizzaStockAPI.Controllers;
+namespace PizzaStoreAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 
 public class PizzaController : ControllerBase
 {
-    public PizzaController()
+    private readonly PizzaDbContext _context; 
+    public PizzaController(PizzaDbContext context)
     {
-
+        _context = context; 
     }
 
     [HttpGet]
-    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+    public async Task<ActionResult<List<Pizza>>> GetAll()
+    {
+        var pizzas = await _context.Pizzas.ToListAsync(); 
+
+        if (pizzas == null || pizzas.Count == 0)
+        {
+            return NotFound("Aucune pizza trouvée"); 
+        }
+
+        return pizzas; 
+
+    }
+
 
     [HttpGet("{id}")]
 
-    public ActionResult<Pizza> Get(int id)
+    public async Task<ActionResult<Pizza>> Get(int id)
     {
-        var pizza = PizzaService.Get(id);
+        var pizza = await _context.Pizzas.FindAsync(id);
 
         if (pizza == null) return NotFound();
 
