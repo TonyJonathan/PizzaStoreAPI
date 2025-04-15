06de9c39bbfc5503dbb5758983c3416ea7218dd2
Tony <tony.bruchon@hotmail.fr>
2025-04-15 18:36:43 +0200

    Create shared project and move Pizza model into shared namespace

---
+++ PizzaStoreApp.Shared/shared/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaStoreApp.Shared.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+    public decimal Price { get; set; }
+}
