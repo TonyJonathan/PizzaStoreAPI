78237c1f9566df81ada9d9401f8c2e598037e06c
Tony <tony.bruchon@hotmail.fr>
2025-04-15 18:35:58 +0200

    Add PizzaService to encapsulate API access in Blazor client

---
+++ PizzaStoreApp.Client/Services/PizzaServices.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using PizzaStoreApp.Shared.Models;
+
+namespace PizzaStoreApp.Client.Services;
+
+public class PizzaService
+{
+    private readonly HttpClient _http;
+
+    public PizzaService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<Pizza>> GetAllAsync()
+    {
+        return await _http.GetFromJsonAsync<List<Pizza>>("https://localhost:7016/api/pizza")
+               ?? new List<Pizza>();
+    }
+}
