546ca6ace67028b4a3dcc3e4b201990d09ee51b1
Tony <tony.bruchon@hotmail.fr>
2025-04-17 23:15:30 +0200

    Add dedicated CSS file and improve overall page styling

--- PizzaStoreApp.Client/obj/Debug/net9.0/PizzaStoreApp.Client.AssemblyInfo.cs
+++ PizzaStoreApp.Client/obj/Debug/net9.0/PizzaStoreApp.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2bc86facfe01a198824a424518c9823f528177a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e3cc5103919541d3e235032d9ca3856ac5fb1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
