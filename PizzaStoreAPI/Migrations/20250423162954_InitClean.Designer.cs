a39db0d7ba4aee2be95db848f4d828825204abc9
Tony <tony.bruchon@hotmail.fr>
2025-04-23 18:31:43 +0200

    Test n°11

--- PizzaStoreAPI/Migrations/20250423162954_InitClean.Designer.cs
+++ PizzaStoreAPI/Migrations/20250423162954_InitClean.Designer.cs
@@ -11,7 +11,7 @@ using PizzaStoreAPI.Data;
 namespace PizzaStoreAPI.Migrations
 {
     [DbContext(typeof(PizzaDbContext))]
-    [Migration("20250423162418_InitClean")]
+    [Migration("20250423162954_InitClean")]
     partial class InitClean
     {
         /// <inheritdoc />
