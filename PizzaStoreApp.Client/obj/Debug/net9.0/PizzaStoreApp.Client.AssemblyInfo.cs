fd6cee864c3ccf73fa0471cf7be32d5ca628df76
Tony <tony.bruchon@hotmail.fr>
2025-04-25 16:02:36 +0200

    Disable edit and delete buttons when showForm is true

--- PizzaStoreApp.Client/obj/Debug/net9.0/PizzaStoreApp.Client.AssemblyInfo.cs
+++ PizzaStoreApp.Client/obj/Debug/net9.0/PizzaStoreApp.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72826e5f162aa16530cce94d1941884023624bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23a36377fcb3b50129f0a4469012bf5bcd95b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStoreApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
