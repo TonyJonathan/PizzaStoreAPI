361a2c4bb21ca27c4898bc79e09480f68a12c57e
Tony <tony.bruchon@hotmail.fr>
2025-05-01 22:17:39 +0200

    Fix: Correct double 'https://' in HttpClient BaseAddress causing API fetch failure

--- PizzaStoreApp.Client/Services/PizzaService.cs
+++ PizzaStoreApp.Client/Services/PizzaService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Json;
 using PizzaStoreApp.Shared.Models;
+using static System.Net.WebRequestMethods;
 
 namespace PizzaStoreApp.Client.Services;
 
@@ -20,7 +21,7 @@ public class PizzaService
 
     public async Task CreateAsync(Pizza pizza)
     {
-        var response = await _http.PostAsJsonAsync("https://https://localhost:7016/api/pizza", pizza);
+        var response = await _http.PostAsJsonAsync("https://localhost:7016/api/pizza", pizza);
         response.EnsureSuccessStatusCode();
     }
 
