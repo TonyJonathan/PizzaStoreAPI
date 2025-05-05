8fbcbcb105dbba4a282c68631670ca2910be12dd
Tony <tony.bruchon@hotmail.fr>
2025-05-05 18:06:24 +0200

    Add UserService

---
+++ PizzaStoreApp.Client/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using PizzaStoreApp.Shared.Models;
+using System.Net.Http.Json;
+
+public class UserService
+{
+    private readonly HttpClient http; 
+
+    public UserService(HttpClient http)
+    {
+        this.http = http; 
+    }
+
+    public async Task<string?> RegisterAsync(RegisterUserDto user)
+    {
+        var response = await http.PostAsJsonAsync("api/user", user); 
+
+        if(response.IsSuccessStatusCode)
+        {
+            return null; 
+        }
+
+        return await response.Content.ReadAsStringAsync(); 
+    }
+}
