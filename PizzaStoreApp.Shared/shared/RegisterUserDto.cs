045d28be8f5ddcb9a63c75b3df6ed21f63e49b2a
Tony <tony.bruchon@hotmail.fr>
2025-05-05 10:37:53 +0200

    Add RegisterUserDto with validation for email and password

---
+++ PizzaStoreApp.Shared/shared/RegisterUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaStoreApp.Shared.Models;
+
+public class RegisterUserDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+
